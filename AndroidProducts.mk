000f203a14f4be27a0c36eb9f65709c547d43126
XTENSEI <andriegalutera@gmail.com>
2026-01-26 17:55:24 +0800

    Initial check-in of remaining tree files

---
+++ AndroidProducts.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/pb_X6855.mk
+
+COMMON_LUNCH_CHOICES := \
+    pb_X6855-userdebug
